96aab939f63b0bfecb2a575e0005e589c8327282
hkmusicmatch <hkmusicmatch@gmail.com>
2011-01-03 19:08:26 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'ef02aaf447849310cf3a51acd49df843aa16bc3590ca5ec3cd38cddd94a5651376b2347d692e4bb1ce81c636a95ba3c734afcc48ba2207de50abcd2eea0df6b6'
